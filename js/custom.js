4e6cd6c4bee29c0d2656b281ddf1bc912acc6229
Imtiaz Fahim <61643273+imtiazfahim256@users.noreply.github.com>
2020-04-10 15:44:14 +0600

    install now
    every thing now insert

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+function button(){
+    document.querySelector('.nav-button').classList.toggle('button-active');
+}
+
+
+// for(var i=0;i<=500;i++){
+   
+//     console.log('.padding-md-top-'+i+'{padding-top:'+i+'px;}');
+   
+// }
+// for(var i=0;i<=100;i++){
+    
+//     console.log('.padding-md-top-'+i+'pre{padding-top:'+i+'%;}');
+   
+// }
+
+// for(var i=0;i<=500;i++){
+  
+//     console.log('.padding-md-bottom-'+i+'{padding-bottom:'+i+'px;}');
+   
+// }
+// for(var i=0;i<=100;i++){
+   
+//     console.log('.padding-md-bottom-'+i+'pre{padding-bottom:'+i+'%;}');
+   
+// }
+
+// for(var i=0;i<=500;i++){
+   
+//     console.log('.padding-md-right-'+i+'{padding-right:'+i+'px;}');
+   
+// }
+// for(var i=0;i<=100;i++){
+   
+//     console.log('.padding-md-right-'+i+'pre{padding-right:'+i+'%;}');
+   
+// }
+// for(var i=0;i<=500;i++){
+   
+//     console.log('.padding-md-left-'+i+'{padding-left:'+i+'px;}');
+   
+// }
+// for(var i=0;i<=100;i++){
+  
+//     console.log('.padding-md-left-'+i+'pre{padding-left:'+i+'%;}');
+   
+// }
